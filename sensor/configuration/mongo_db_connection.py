e54ca86619771a88a5e88f613bf0df25f919c47f
Shiva <t.shiva548@gmail.com>
2022-11-05 23:35:36 +0530

    done

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,8 +1,10 @@
+import pymongo
 import os
 import sys
 import certifi
-import pymongo
-from sensor.constants import DATABASE_NAME
+from sensor.constant.database import DATABASE_NAME
+from  sensor.exception import SensorException
+
 
 ca = certifi.where()
 
@@ -13,7 +15,11 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv("mongodb+srv://shiva:12345@cluster0.ntojkjj.mongodb.net/test")
+                
+                mongo_db_url ="mongodb+srv://shiva:12345@cluster0.ntojkjj.mongodb.net/test"
+                #mongo_db_url="mongodb+srv://avnish:Aa327030@ineuron-ai-projects.7eh1w4s.mongodb.net/?retryWrites=true&w=majority"
+                #mongodb+srv://avnish:Aa327030@ineuron-ai-projects.7eh1w4s.mongodb.net/?retryWrites=true&w=majority"
+                
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
             self.client = MongoDBClient.client
